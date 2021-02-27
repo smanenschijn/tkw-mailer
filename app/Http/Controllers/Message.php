c7410f686b9763e8f83738eb142eeae5524547a6
Stefan Manenschijn <smanenschijn@me.com>
2021-02-27 12:16:37 +0100

    added basic validation in controller

--- app/Http/Controllers/Message.php
+++ app/Http/Controllers/Message.php
@@ -3,12 +3,34 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
 class Message extends Controller
 {
 
-    public function createMessage()
+    public function createMessage(Request $request)
     {
-        return response()->json(['success' => true]);
+        try {
+            $messageData = json_decode($request->getContent(), true);
+
+            $validator = Validator::make($messageData, [
+                'recipients' => 'required|array',
+                'subject' => 'required',
+                'body' => 'required'
+            ]);
+
+            if ($validator->fails()) {
+                throw new ValidationException($validator);
+            }
+
+            return response()->json(['success' => true]);
+
+        } catch (ValidationException $validationException) {
+            return response()->json(['errors' => $validator->errors()], 500);
+        }
+
     }
 
 }
